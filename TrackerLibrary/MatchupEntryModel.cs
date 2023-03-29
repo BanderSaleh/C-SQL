4b64bd8c80bef40b0f884a955349c2c6344cc6bf
Bander Saleh <bandersaleh93@gmail.com>
2023-03-29 14:48:14 -0700

    Initial commit

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        // Properties
+
+        /// <summary>
+        /// Represents one team in the matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents the score for this particular team.
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Represents the matchup that this team came 
+        /// from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+        
+        // Constructor
+        //test code
+        public MatchupEntryModel(double initialScore)
+        {
+            Console.WriteLine(initialScore);
+        }
+
+
+
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();//Initialize our new List
+        public string TeamName { get; set; }
+
+
+        
+        
+    }
+}
